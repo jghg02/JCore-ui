7730e41ac445aed4aebfa2f963ab9c91eed28ff6
Josue Hernandez <jghg.02@gmail.com>
2018-04-24 23:19:46 -0300

    Initial Commit.

---
+++ JCore-ui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JCore-ui
+//
+//  Created by Josue Hernandez Gonzalez on 24/04/2018.
+//  Copyright © 2018 Josue Hernandez Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JCore-uiTests/JCore_uiTests.swift
@@ -0,0 +1,36 @@
+//
+//  JCore_uiTests.swift
+//  JCore-uiTests
+//
+//  Created by Josue Hernandez Gonzalez on 24/04/2018.
+//  Copyright © 2018 Josue Hernandez Gonzalez. All rights reserved.
+//
+
+import XCTest
+@testable import JCore_ui
+
+class JCore_uiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
